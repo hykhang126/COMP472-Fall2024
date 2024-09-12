b6cdeb414222cd80511883e8f988d2158bea4136
Hy Khang Tran <106112821+hykhang126@users.noreply.github.com>
2024-09-12 18:04:10 -0400

    Add files via upload

--- Lab/Lab_1/Agent.py
+++ Lab/Lab_1/Agent.py
@@ -12,26 +12,40 @@ class Agent:
         self.posY = 0
         
     def move(self, direction):
+		# add bounds checking to make sure agent does not go out of the grid
         if direction == "up":
             self.posX -= 1
+            if self.posX < 0:
+                self.posX += 1
+                self.move("down")
         elif direction == "down":
             self.posX += 1
+            if self.posX >= self.grid.n:
+                self.posX -= 1
+                self.move("up")
         elif direction == "left":
             self.posY -= 1
+            if self.posY < 0:
+                self.posY += 1
+                self.move("right")
         elif direction == "right":
             self.posY += 1
+            if self.posY >= self.grid.n:
+                self.posY -= 1
+                self.move("left")
         else:
             print("Invalid direction.")
         
     def clean(self):
-        if self.grid.get_cell(self.posX, self.posY) == Room.DIRTY:
-            self.grid.update_cell(self.posX, self.posY, Room.CLEAN)
+        if self.grid.get_cell(self.posX, self.posY).state == Room.DIRTY:
+            room = Room(self.posX, self.posY, Room.CLEAN)
+            self.grid.update_cell(self.posX, self.posY, room)
             print(f"Room ({self.posX}, {self.posY}) cleaned.")
         else:
             print(f"Room ({self.posX}, {self.posY}) is already clean.")
     
     def detect(self):
-        if self.grid.get_cell(self.posX, self.posY) == Room.DIRTY:
+        if self.grid.get_cell(self.posX, self.posY).state == Room.DIRTY:
             print(f"Room ({self.posX}, {self.posY}) is dirty.")
             return True
         else:
